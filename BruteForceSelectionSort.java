5057f4140a6f9ffe2f09d8196fc043a56d2ac6bc
krits29 <31025768+krits29@users.noreply.github.com>
2022-07-19 10:51:41 -0700

    Basic Selection Sort

---
+++ BruteForceSelectionSort.java
@@ -0,0 +1,57 @@
+// Brute force selection sort
+// - simplist design strategy
+// - most straigthforward approach
+// - given an input of numbers 
+// - output a permutation of the sequence of numbers
+// - increasing order of numbers
+// - Algorithm:
+// - locate the smallest item and put it in the first place
+// - then locate the next smallest and put it int he second plate
+// - sort by repeated selection
+
+public class BruteForceSelectionSort 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+
+        int[] unsorted = {6, 4, 5, 3, 5, 8};
+
+        System.out.println("unsorted");
+        for(int i = 0; i < unsorted.length; i++)
+        {
+            System.out.print(unsorted[i] + " ");
+        }
+
+        int[] sorted = new int[] selectionSort(unsorted);
+
+        System.out.println("sorted");
+        for(int i = 0; i < sorted.length; i++)
+        {
+            System.out.print(sorted[i] + " ");
+        }
+    }
+
+    public static int[] selectionSort(int[] unsorted)
+    {
+        
+        for(int i = 0; i < unsorted.length; i++)
+        {
+            int minValue = unsorted[i];
+            int minIndex = i;
+            for(int j = i + 1; j < unsorted.length; j++)
+            {
+                if(unsorted[j] < minValue)
+                {
+                    minValue = unsorted[j];
+                    minIndex = j;
+                }
+            }
+            int temp = unsorted[i];
+            unsorted[i] = unsorted[minIndex];
+            unsorted[minIndex] = temp;
+        }
+        return unsorted;
+        
+    }
+}
